a40e81da1651287c4e3a8b19b673c05169d2da72
shine <shine@atguigu.com>
2023-08-23 22:07:33 +0800

    first commit

---
+++ src/main/java/git/gitTest.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * @author txy
+ * @description
+ * @date 2023/8/23 - 22:02
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
